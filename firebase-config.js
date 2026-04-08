072109b193f3d4841fb881eb4b26ff86d62b33f2
a-mitt <165976185+a-mitt@users.noreply.github.com>
2026-04-08 10:09:06 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmkTw5LootQuP1gre_tQf8vAZdHYyIrh0",
+  authDomain: "my-timetable-9469e.firebaseapp.com",
+  projectId: "my-timetable-9469e",
+  storageBucket: "my-timetable-9469e.firebasestorage.app",
+  messagingSenderId: "328653144512",
+  appId: "1:328653144512:web:93cff01e25f2a985a2c825"
+};
